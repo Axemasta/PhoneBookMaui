9189bdf86b50ebe610bbd00ebb3f5c55fb83ba5e
Axemasta <33064621+Axemasta@users.noreply.github.com>
2021-12-14 22:32:08 +0000

    Updated contact list ui
    Made this ui on the contact page look much prettier!

---
+++ src/PhoneBookApp/Controls/CircularFrame.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using System.Runtime.CompilerServices;
+
+namespace PhoneBookApp.Controls
+{
+    public class CircularFrame : Frame
+    {
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == nameof(Height))
+            {
+                this.WidthRequest = Height;
+                
+                if (float.TryParse(Height.ToString(), out float fHeight))
+                {
+                    this.CornerRadius = fHeight / 2;
+                }
+            }
+        }
+    }
+}
---
+++ src/PhoneBookApp/Converters/ContactInitialsConverter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Controls;
+using PhoneBookApp.Models;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace PhoneBookApp.Converters
+{
+    public class ContactInitialsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return null;
+
+            if (value is not Contact contact)
+            {
+                throw new NotSupportedException($"Value must be of type {nameof(Contact)}");
+            }
+
+            string initals = string.Empty;
+
+            if (!string.IsNullOrEmpty(contact.FirstName))
+            {
+                initals += contact.FirstName.FirstOrDefault();
+            }
+
+            if (!string.IsNullOrEmpty(contact.LastName))
+            {
+                initals += contact?.LastName.FirstOrDefault();
+            }
+
+            return initals.ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
